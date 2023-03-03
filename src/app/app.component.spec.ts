6bf5419485541f4dc000e48239cde955f5efde53
akashvgeorge <akashviji1775@gmail.com>
2023-03-03 12:27:35 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pj1'`, () => {
+  it(`should have as title 'project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('pj1');
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pj1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('project app is running!');
   });
 });
---
+++ src/assets/districts.ts
@@ -0,0 +1,99 @@
+export const districts= [
+  {
+      name: "Trivandrum",
+      desc: "Thiruvananthapuram (or Trivandrum) is the capital of the southern Indian state of Kerala. . ",
+      Image:"https://www.happytrips.com/photo/57891680.cms",
+      more:"https://en.wikipedia.org/wiki/Thiruvananthapuram",
+
+  },
+  {
+      name: " Alappuzha,",
+      desc: "Alappuzha (or Alleppey) is a city on the Laccadive Sea in the southern Indian state of Kerala. . ",
+      Image:"https://images.unsplash.com/photo-1593693411515-c20261bcad6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YWxhcHB1emhhfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      more:"https://en.wikipedia.org/wiki/Alappuzha",
+
+  },
+  {
+      name: "Ernakulam",
+      desc: "Sprawling, residential Ernakulam is known for Marine Drive, a busy waterfront promenade.  . ",
+      Image:"https://im.hunt.in/cg/City-Guide/about-erna.jpg",
+      more:"https://en.wikipedia.org/wiki/Ernakulam",
+
+  },
+  {
+      name: "Palakkad,",
+      desc: "Palakkad, or Palghat, is a city in Kerala, a state in southwestern India. ",
+      Image:"https://images.unsplash.com/photo-1553448457-df92e62f004b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGFsYWtrYWR8ZW58MHx8MHx8&w=1000&q=80",
+      more:"https://en.wikipedia.org/wiki/Palakkad",
+
+  },
+  {
+      name: "Kollam",
+      desc: "Kollam is a city in the state of Kerala, on India's Malabar Coast. It’s known as a trade hub and for its beaches. . ",
+      Image:"https://img.traveltriangle.com/blog/wp-content/uploads/2018/04/Jatayu%E2%80%99s-Earth-Centre-kollam-kb6592.jpg",
+      more:"https://en.wikipedia.org/wiki/Kollam",
+
+  },
+  {
+      name: "Kannur",
+      desc: "Kannur is a coastal city in the south Indian state of Kerala. ",
+      Image:"https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2019/03/26/Fort%20Kannur-1553562002.JPG?itok=9A1hPaon",
+      more:"https://en.wikipedia.org/wiki/Kannur",
+  },
+  {
+      name: "Kasaragod",
+      desc: "Kasaragod is a municipal town and administrative headquarters of Kasaragod district in the state of Kerala ",
+      Image:"https://theprimetime.in/wp-content/uploads/2021/11/Kasaragod-Kerala.jpg",
+      more:"https://en.wikipedia.org/wiki/Kasargod",
+
+  },
+  {
+      name: "Idukki",
+      desc: "Idukki is a township in Idukki district near to the district headquarter Painavu in the state of Kerala ",
+      Image:"https://keralatourism.travel/images/destinations/headers/idukki-kerala-tourism-entry-fee-timings-holidays-reviews-header.jpg",
+      more:"https://en.wikipedia.org/wiki/Idukki",
+
+  },
+  {
+      name: "Kottayam",
+      desc: "Kottayam is a municipal town in the Indian state of Kerala.",
+      Image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Vembanad_Lake_at_Kumarakom.jpg/1920px-Vembanad_Lake_at_Kumarakom.jpg",
+      more:"https://en.wikipedia.org/wiki/Kottayam",
+
+  },
+  {
+      name: "Thrissur",
+      desc: "Thrissur is a city in the south Indian state of Kerala ",
+      Image:"https://www.keralatv.in/media/2014/05/Thrissur-Pooram-2014-Live.jpg",
+      more:"https://en.wikipedia.org/wiki/Thrissur",
+
+  },
+  {
+      name: "Pathanamthitta",
+      desc: "Pathanamthitta, is a municipality situated in the Central Travancore region in the state of Kerala ",
+      Image:"https://images.thrillophilia.com/image/upload/s--ppcKGM8c--/c_fill,g_auto,h_600,q_auto,w_975/f_auto,fl_strip_profile/v1/images/photos/000/161/184/original/1562159178_4182500804_855c196b5f_o_(1).jpg.jpg?1562159178",
+      more:"https://en.wikipedia.org/wiki/Pathanamthitta",
+
+  },
+  {
+      name: "Malappuram",
+      desc: "Malappuram is a city in the Indian state of Kerala, spread over an area of 58.20 km² ",
+      Image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Malappuram_DownHill_Aerial_View.jpg/1920px-Malappuram_DownHill_Aerial_View.jpg",
+      more:"https://en.wikipedia.org/wiki/Malappuram",
+
+  },
+  {
+      name: "Wayanad",
+      desc: "Wayanad is a rural district in Kerala state, southwest India. In the east, the Wayanad Wildlife Sanctuary is a lush ",
+      Image:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Blue%2C_Green_%26_White.jpg/1280px-Blue%2C_Green_%26_White.jpg",
+      more:"https://en.wikipedia.org/wiki/Wayanad",
+
+  },
+  {
+      name: "Kozhikode",
+      desc: "Kozhikode is a coastal city in the south Indian state of Kerala. It was a significant spice trade center ",
+      Image:"https://images.indianexpress.com/2021/07/DSC_0031.jpg",
+      more:"https://en.wikipedia.org/wiki/Kozhikode",
+
+  },
+]
